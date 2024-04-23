352a381d2f3379cd540c0c320832c842cc54109e
souzafe13 <souzafe13@gmail.com>
2024-04-23 19:49:23 -0300

    subindo para estudo de js

--- index.html/index.js
+++ index.html/index.js
@@ -1,13 +1,33 @@
 let numero = 0;
-
-
-document.getElementById('texto').innerHTML = numero;
+document.getElementById("texto").innerHTML = numero;
 
 function botao(){
-    let acrescentar = ++numero;
-    document.getElementById("resposta").innerHTML = acrescentar;
+    let aumentar = numero+=1;
+    document.getElementById("resposta").innerHTML = numero;
+
+let valor = 5;
+
+if(numero == valor){
+    var body = document.body;
+    body.style.background = "red";
 }
+else if (numero == 10){
+    var body = document.body;
+    body.style.background = "red";
+}
+else if(numero == 1){
+    var body = document.body;
+    body.style.background = "red";
+}
+
+else{
+    var body = document.body;
+    body.style.background = "green";
+}
+}
+
 function botao2(){
-    let diminuir = --numero;
-    document.getElementById("resposta").innerHTML = diminuir;
+    let diminuir = numero-=1;
+    document.getElementById("resposta").innerHTML = numero
 }
+
